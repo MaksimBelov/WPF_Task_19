119cd73aa81951a9d9241c669e3666860a826ddb
Максим Белов <makbe@yandex.ru>
2022-01-28 23:30:26 +0300

    Добавьте файлы проекта.

---
+++ WPF_Task_19/Models/Circle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Task_19.Models
+{
+    static class Circle
+    {
+        public static double CalcLength(double r) => 2 * Math.PI * r;
+    }
+}
---
+++ WPF_Task_19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WPF_Task_19.Models;
+
+namespace WPF_Task_19.ViewModels
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double length;
+        public double Length
+        {
+            get => length;
+            set
+            {
+                length = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+
+        private void OnAddCommandExecute(object p)
+        {
+            Length = Circle.CalcLength(radius);
+        }
+        private bool CanAddCommandExecut(object p)
+        {
+            if (Radius != 0)
+                return true;
+            else
+                return false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecut);
+        }
+    }
+}
